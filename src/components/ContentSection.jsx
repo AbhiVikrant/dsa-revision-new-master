4641c5c75152a6dd3e3afe9719bce55ee147db07
AbhiVikrant <vikrantabhishek72@gmail.com>
2024-11-15 00:16:49 +0530

    Fixed responsive

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -4,155 +4,157 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const ContentSection = () => {
   return (
     <>
-      <div className="content_section   flex flex-col justify-between sm:mx-8 mt-8  gap-2">
+      <div className="content_section   flex flex-col justify-between sm:mx-8 mt-8  gap-2 ">
         <div className="tile_of_content_section flex justify-center font-bold text-3xl leading-3 font-Quicksand text-color-text  mt-12">
           <h1>What&apos;s Included</h1>
         </div>
-      </div>
-      <div className="book-include-cover gap-2 flex flex-col sm:mx-8">
-        <img src="./assets/images/dsa-2.png" alt="dsa-include-cover" />
-      </div>
-      <div className="list-items flex flex-col justify-center items-center sm:mx-8 mt-8  gap-2">
-        <ul>
-          <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
-            <svg
-              className="svg-inline--fa fa-check-circle fa-w-16 me-2"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="check-circle"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              data-fa-i2svg=""
-            >
-              <path
-                fill="currentColor"
-                d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
-              ></path>
-            </svg>
-            15+ algo patterns discussed
-          </li>
-          <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
-            <svg
-              className="svg-inline--fa fa-check-circle fa-w-16 me-2"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="check-circle"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              data-fa-i2svg=""
-            >
-              <path
-                fill="currentColor"
-                d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
-              ></path>
-            </svg>
-            Detailed solutions and explanations
-          </li>
-          <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
-            <svg
-              className="svg-inline--fa fa-check-circle fa-w-16 me-2"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="check-circle"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              data-fa-i2svg=""
-            >
-              <path
-                fill="currentColor"
-                d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
-              ></path>
-            </svg>
-            Awesome resources, articles, blogs
-          </li>
-          <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium">
-            <svg
-              className="svg-inline--fa fa-check-circle fa-w-16 me-2"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="check-circle"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              data-fa-i2svg=""
-            >
-              <path
-                fill="currentColor"
-                d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
-              ></path>
-            </svg>
-            Visualizers and animations included
-          </li>
-          <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
-            <svg
-              className="svg-inline--fa fa-check-circle fa-w-16 me-2"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="check-circle"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              data-fa-i2svg=""
-            >
-              <path
-                fill="currentColor"
-                d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
-              ></path>
-            </svg>
-            100 pages of quality content
-          </li>
-          <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium">
-            <svg
-              className="svg-inline--fa fa-check-circle fa-w-16 me-2"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="check-circle"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              data-fa-i2svg=""
-            >
-              <path
-                fill="currentColor"
-                d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
-              ></path>
-            </svg>
-            Concepts explained for beginners
-          </li>
-          <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
-            <svg
-              className="svg-inline--fa fa-check-circle fa-w-16 me-2"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="check-circle"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              data-fa-i2svg=""
-            >
-              <path
-                fill="currentColor"
-                d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
-              ></path>
-            </svg>{" "}
-            All in one revision guide
-          </li>
-        </ul>
-      </div>
-      <div className="download_button flex justify-center">
-        <button className="bg-orange-500 text-white text-2xl from-neutral-600 text-center px-8 py-4 rounded-full font-Quicksand mb-4">
-          Download for free
-        </button>
+        <div className=" flex flex-col sm:mx-8  mt-8  gap-2 lg:grid lg:grid-cols-2">
+          <div className="book-include-cover  ">
+            <img src="./assets/images/dsa-2.png" alt="dsa-include-cover" />
+          </div>
+          <div className="list-items flex flex-col justify-center items-center sm:mx-8 mt-8  gap-2">
+            <ul>
+              <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
+                <svg
+                  className="svg-inline--fa fa-check-circle fa-w-16 me-2"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="check-circle"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                  ></path>
+                </svg>
+                15+ algo patterns discussed
+              </li>
+              <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
+                <svg
+                  className="svg-inline--fa fa-check-circle fa-w-16 me-2"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="check-circle"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                  ></path>
+                </svg>
+                Detailed solutions and explanations
+              </li>
+              <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
+                <svg
+                  className="svg-inline--fa fa-check-circle fa-w-16 me-2"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="check-circle"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                  ></path>
+                </svg>
+                Awesome resources, articles, blogs
+              </li>
+              <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium">
+                <svg
+                  className="svg-inline--fa fa-check-circle fa-w-16 me-2"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="check-circle"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                  ></path>
+                </svg>
+                Visualizers and animations included
+              </li>
+              <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
+                <svg
+                  className="svg-inline--fa fa-check-circle fa-w-16 me-2"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="check-circle"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                  ></path>
+                </svg>
+                100 pages of quality content
+              </li>
+              <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium">
+                <svg
+                  className="svg-inline--fa fa-check-circle fa-w-16 me-2"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="check-circle"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                  ></path>
+                </svg>
+                Concepts explained for beginners
+              </li>
+              <li className="text-color-text text-xl mb-3 leading-loose font-Quicksand font-medium ">
+                <svg
+                  className="svg-inline--fa fa-check-circle fa-w-16 me-2"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="check-circle"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                  ></path>
+                </svg>{" "}
+                All in one revision guide
+              </li>
+            </ul>
+            <div className="download_button flex justify-center">
+              <button className="bg-orange-500 text-white text-2xl from-neutral-600 text-center px-8 py-4 rounded-full font-Quicksand mb-4">
+                Download for free
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -3,9 +3,9 @@ import Hero from "./Hero";
 const IntroSection = () => {
   return (
     <>
-      <div className="mainDiv flex flex-col justify-between sm:mx-8 ">
+      <div className="mainDiv flex flex-col justify-between sm:mx-8 lg:grid lg:grid-cols-2 sm:grid sm:grid-cols-2">
         <div>
-          <div className="introStart gap-2 flex flex-col lg:flex lg:flex-col  ">
+          <div className="introStart gap-8 flex flex-col lg:flex lg:flex-col lg:my-16 sm:flex sm:flex-col sm:my-16 ">
             <h1 className="font-lato font-bold text-4xl  justify-between leading-tight">
               Best way to revise for your next coding interview
             </h1>
@@ -16,16 +16,20 @@ const IntroSection = () => {
             </p>
           </div>
 
-          <div className="  gumroad flex justify-center items-center text-center bg-slate-950 text-white border-solid border-8 border-black rounded-md font-lato px-2 py-3 mt-8 hover:bg-purple-btn hover:border-purple-btn hover:ease-out hover:transition-transform hover:-translate-x-1 hover:-translate-y-1 hover:shadow-lg hover:shadow-black hover:text-black active:bg-red-btn active:border-red-btn  lg:flex lg:flex-col">
+          <div className="  gumroad flex justify-center items-center text-center bg-slate-950 text-white border-solid border-8 border-black rounded-md font-lato px-2 py-3 mt-8 hover:bg-purple-btn hover:border-purple-btn hover:ease-out hover:transition-transform hover:-translate-x-1 hover:-translate-y-1 hover:shadow-lg hover:shadow-black hover:text-black active:bg-red-btn active:border-red-btn  lg:flex lg:flex-col lg:my-10 sm:flex sm:flex-col sm:my-10">
             <button>Buy FREE through Gumroad</button>
           </div>
-          <div className=" hero gap-2 block mt-10   lg:flex lg:flex-col">
+          <div className=" hero gap-2 block mt-10  sm:my-10 sm:flex sm:flex-col lg:my-10 lg:flex lg:flex-col">
             <Hero />
           </div>
         </div>
         <div>
-          <div className="book-cover-holder gap-2 flex flex-col mt-10 relative">
-            <img src="./assets/images/dsa-main.png" alt="dsa-main-cover" />
+          <div className="book-cover-holder gap-2 flex flex-col mt-8 sm:mx-8 lg:my-0 relative lg:mx-24 ">
+            <img
+              src="./assets/images/dsa-main.png"
+              alt="dsa-main-cover"
+              className=""
+            />
             <div className="book-badges absolute  h-[100px] w-[100px]   pt-[25px]  right-0 -top-8 bg-green-light rounded-[50%] text-xl leading-6 text-center font-Quicksand font-bold text-white">
               New
               <br />
